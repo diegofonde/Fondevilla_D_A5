fd706b64266901af88c112863e054a5ac855d56b
Diego Fondevilla <125434260+diegofonde@users.noreply.github.com>
2024-05-06 00:37:22 -0700

    Create Records.cpp

---
+++ Records.cpp
@@ -0,0 +1,39 @@
+#include "Records.h"
+
+Records::Records() {
+    id = 0;
+    name = "";
+    level = "";
+}
+
+Records::Records(int i) {
+    id = i;
+    name = "";
+    level = "";
+}
+
+Records::Records(int i, string s, string s2) {
+    id = i;
+    name = s;
+    level = s2;
+}
+
+Records::~Records() {
+    //to do
+}
+
+bool Records::isEquals(Records *r) {
+    return(id == r->getId());
+}
+
+bool Records::isGreater(Records *r) {
+    return(id > r->getId());
+}
+
+bool Records::isLesser(Records *r) {
+    return(id < r->getId());
+}
+
+id Records::getId() {
+    return id;
+}
