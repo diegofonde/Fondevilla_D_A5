b5f63df1b3f292018f7d97a98568638ef6336d6a
Diego Fondevilla <125434260+diegofonde@users.noreply.github.com>
2024-05-06 00:37:51 -0700

    Create Students.h

---
+++ Students.h
@@ -0,0 +1,18 @@
+#ifndef STUDENT_H
+#define STUDENT_H
+
+#include "Records.h"
+
+class Student: public Records {
+    public:
+        Student();
+        Student(int i, string s, string s2, string s3, double g, int a);
+        ~Student();
+    private:
+        string major;
+        double GPA;
+        int advisor;
+
+};
+
+#endif
