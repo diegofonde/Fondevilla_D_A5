5148311ec7fe267b2c2601ddb3bc14fa5e768f2e
Diego Fondevilla <125434260+diegofonde@users.noreply.github.com>
2024-05-06 00:36:56 -0700

    Create Records.h

---
+++ Records.h
@@ -0,0 +1,24 @@
+#ifndef RECORDS_H
+#define RECORDS_H
+
+#include <iostream>
+using namespace std;
+
+class Records{
+    public:
+        Records(); //default constructor
+        Records(int i); //overloaded constrcutor for only id
+        Records(int i, string s, string s2); //overloaded constructor for every variable
+        ~Records(); //default constructor
+        bool isEquals(Records *r);
+        bool isGreater(Records *r);
+        bool isLesser(Records *r);
+        int getId();
+    protected:
+        int id; //int variable for id
+        string name; //string variable for name
+        string level; //string variable for level
+
+};
+
+#endif
