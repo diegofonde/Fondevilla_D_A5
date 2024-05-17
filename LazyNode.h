b62a5726773c94accaeaead7eeb476326811aad2
Diego Fondevilla <125434260+diegofonde@users.noreply.github.com>
2024-05-17 02:10:13 -0700

    update LazyNode

--- LazyNode.h
+++ LazyNode.h
@@ -7,16 +7,42 @@
 #include "Faculty.h"
 using namespace std;
 
+template <class T>
 class LazyNode {
     public:
         LazyNode();
-        LazyNode(Records *r);
+        LazyNode(T k);
         virtual ~LazyNode();
 
-        int id;
-        Records *key;
+        T key;
         LazyNode *left;
         LazyNode *right;
+        int leftDepth;
+        int rightDepth;
 };
+
+template <class T>
+LazyNode<T> :: LazyNode() {
+    key = NULL;
+    left = NULL;
+    right = NULL;
+    leftDepth = 0;
+    rightDepth = 0;
+}
+
+template <class T>
+LazyNode<T> :: LazyNode(T k) {
+    key = k;
+    left = NULL;
+    right = NULL;
+    leftDepth = 0;
+    rightDepth = 0;
+}
+
+template <class T>
+LazyNode<T> :: ~LazyNode() {
+    left = NULL;
+    right = NULL;
+}
 
 #endif
