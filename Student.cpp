55608c3bd7cb660d34d7edc7dc56304df6870c6e
Diego Fondevilla <125434260+diegofonde@users.noreply.github.com>
2024-05-17 23:23:49 -0700

    finish student

--- Student.cpp
+++ Student.cpp
@@ -1,24 +1,49 @@
 #include "Student.h"
 
-Student :: Student() : Records() {
+Student :: Student() : Records() { //default constructor
     major = "";
     GPA = 0.0;
     advisor = 0;
 }
 
-Student :: Student(int i, string s, string s2, string s3, double g, int a) : Records(i, s, s2) {
+Student :: Student(int i) : Records(i) { //overloaded constructor for id 
+    major = "";
+    GPA = 0.0;
+    advisor = 0;
+}
+
+Student :: Student(int i, string s, string s2, string s3, double g) : Records(i, s, s2) { //overloaded constructor for every variable except advisor
+    major = s3;
+    GPA = g;
+    advisor = 0;
+}
+
+Student :: Student(int i, string s, string s2, string s3, double g, int a) : Records(i, s, s2) { //overloaded constructor for every variable
     major = s3;
     GPA = g;
     advisor = a;
 }
 
-Student :: ~Student() {
-    //to do
+Student :: ~Student() { // deconstructor
+    cout << "Delete Student" << endl;
 }
 
-void Student :: printInfo() {
+void Student :: printInfo() const{  //print method for student
     Records :: printInfo();
     cout << "Major: " << major << endl;
     cout << "GPA: " << GPA << endl;
-    cout << "Advisor: " << advisor << endl;
+    if(advisor == 0) {
+        cout << "Advisor: " << endl;
+    }
+    else {
+        cout << "Advisor: " << advisor << endl;
+    }
+}
+
+void Student :: setAdvisor(int i) {  //void method for setting the advisor of the student
+    advisor = i;
+}
+
+int Student :: getAdvisor() { //getter method to return the advisor variable
+    return advisor;
 }
--- Student.h
+++ Student.h
@@ -5,14 +5,18 @@
 
 class Student: public Records {
     public:
-        Student();
-        Student(int i, string s, string s2, string s3, double g, int a);
-        ~Student();
-        void printInfo() override;
+        Student(); //default constructor
+        Student(int i); //overloaded constructor for id
+        Student(int i, string s, string s2, string s3, double g); //overloaded constructor for every variable except advisor
+        Student(int i, string s, string s2, string s3, double g, int a); //overloaded constructor for every variable
+        void setAdvisor(int i); //void method for setting the advisor of the student
+        ~Student(); // deconstructor
+        int getAdvisor(); //getter method to return the advisor variable
+        void printInfo() const override; //print method for student
     private:
-        string major;
-        double GPA;
-        int advisor;
+        string major; //string variable for major 
+        double GPA; //double variable for GPA
+        int advisor; //int variable for advisor
 
 };
 
