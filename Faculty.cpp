a8197ec221ea3db947e2a5f3b7a0742cba5e9e6f
Diego Fondevilla <125434260+diegofonde@users.noreply.github.com>
2024-05-18 04:04:35 -0700

    Finished Faculty

--- Faculty.cpp
+++ Faculty.cpp
@@ -1,18 +1,40 @@
 #include "Faculty.h" 
 
-Faculty :: Faculty() : Records() {
+Faculty :: Faculty() : Records() { //default constructor
     department = "";
 }
 
-Faculty :: Faculty(int i, string s, string s2, string s3) : Records(i, s, s2) {
+
+Faculty :: Faculty(int i) : Records(i) { //overloaded constructor for id
+    department = "";
+}
+
+Faculty :: Faculty(int i, string s, string s2, string s3) : Records(i, s, s2) { //overloaded constructor for all variavkes
     department = s3;
 }
 
-Faculty :: ~Faculty() {
-    //to do
+Faculty :: ~Faculty() { //deconstructor
+    cout << "Delete Faculty" << endl;
 }
 
-void Faculty :: printInfo() {
+void Faculty :: addAdvisee(int i) {
+    advisees.push_back(i);
+}
+
+void Faculty :: removeAdvisee(int i) {
+    advisees.remove(i);
+}
+
+list<int> Faculty :: getAdvisees() {
+    return advisees;
+}
+
+void Faculty :: printInfo() const {
     Records :: printInfo();
     cout << "Department: " << department << endl;
+    cout << "Advisees: ";
+    for(const auto& id : advisees) {
+        cout << id << " ";
+    }
+    cout << endl;
 }
--- Faculty.h
+++ Faculty.h
@@ -2,15 +2,21 @@
 #define FACULTY_H
 
 #include "Records.h"
+#include <list>
 
 class Faculty: public Records {
     public:
-        Faculty();
-        Faculty(int i, string s, string s2, string s3);
-        ~Faculty();
-        void printInfo() override;
+        Faculty(); //default constructor
+        Faculty(int i); //overloaded method for id
+        Faculty(int i, string s, string s2, string s3); //overloaded method for all variables
+        ~Faculty(); //deconstructor method
+        void addAdvisee(int i); //method to add advisee to the list
+        void removeAdvisee(int i); //method to remove advisee from the list
+        void printInfo() const override; //print method for Faculty
+        list<int> getAdvisees(); //get list method
     private:
-        string department;
+        string department; //string variable for department
+        list<int> advisees; //list variable to keep track of all advisees
 
 };
 
